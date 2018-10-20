80159221835e8e9ca4cd022d7826a4c99b546c93
Zygimantas Kairys <zkairyas@neweraed.co.uk>
2018-10-20 15:00:57 +0100

    some html and files created

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shout Box</title>
+    <link rel="stylesheet" href="styles.css" type="text/css">
+</head>
+<body>
+    <section class="shout">
+        <div class="shout-box">
+            <div class="shout-box-container">
+                <header class="shout-box-header">
+                    <h1 class="shout-box-header-title">ShoutBox</h1>
+                </header>
+                <div class="shout-box-shouts">
+                    <ul class="shout-box-shouts-all">
+                        <li class="shout-box-shouts-all-single">
+                            <span class="shout-box-shouts-all-single-time">
+                                10:15 -
+                            </span>
+                            <strong>ZK: What up?</strong>
+
+                        </li>
+                        <li class="shout-box-shouts-all-single">
+                            <span class="shout-box-shouts-all-single-time">
+                                10:15 -
+                            </span>
+                            <strong>ZK: What up?</strong>
+
+                        </li>
+                        <li class="shout-box-shouts-all-single">
+                            <span class="shout-box-shouts-all-single-time">
+                                10:15 -
+                            </span>
+                            <strong>ZK: What up?</strong>
+
+                        </li>
+                        <li class="shout-box-shouts-all-single">
+                            <span class="shout-box-shouts-all-single-time">
+                                10:15 -
+                            </span>
+                            <strong>ZK: What up?</strong>
+
+                        </li>
+                        <li class="shout-box-shouts-all-single">
+                            <span class="shout-box-shouts-all-single-time">
+                                10:15 -
+                            </span>
+                            <strong>ZK: What up?</strong>
+
+                        </li>
+                    </ul>
+                </div>
+                <div clas="shout-box-form">
+                    <form action="post" action="process.php">
+                        <input type="text" name="user" placeholder="Name">
+                        <input type="text" name="message" placeholder="Shout">
+                        <input type="submit" name="submit" value="Shout It!">   
+                    </form>
+                </div>
+        
+            </div> 
+        </div>
+    </section>
+</body>
+</html>
